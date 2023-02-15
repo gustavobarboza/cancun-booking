2fb8d32ecae1a2f87238f59f398cc48f1feed0b0
Gustavo Barboza <gustavobarboza.oliveira@gmail.com>
2023-02-15 23:21:45 +0000

    Initial project.

---
+++ src/main/java/com/gustavo/cancunbooking/CancunbookingApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.cancunbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CancunbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CancunbookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavo/cancunbooking/CancunbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.cancunbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CancunbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
